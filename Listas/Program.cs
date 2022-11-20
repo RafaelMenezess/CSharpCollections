67c0a59601b8f84859cf74413b28906c8422aaec
Rafael Menezes <rafa.rcm@hotmail.com>
2022-11-20 17:04:21 -0300

    Operações com Listas

--- Listas/Program.cs
+++ Listas/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Listas
 {
@@ -27,13 +28,38 @@ namespace Listas
             Imprimir(aulas);
 
             Console.WriteLine("A primeira aula é " + aulas[0]);
-            Console.WriteLine("A última aula é " + aulas[aulas.Count -1]);
+            Console.WriteLine("A última aula é " + aulas[aulas.Count - 1]);
 
             Console.WriteLine("A primeira aula é " + aulas.First());
             Console.WriteLine("A última aula é " + aulas.Last());
 
+            aulas[0] = "Trabalhando com Listas";
+            Imprimir(aulas);
 
+            Console.WriteLine("A primeira aula 'Trabalhando' é: " + aulas.First(aula => aula.Contains("Trabalhando")));
+            Console.WriteLine("A última aula 'Trabalhando' é: " + aulas.Last(aula => aula.Contains("Trabalhando")));
+            Console.WriteLine("A primeira aula 'Conclusão' é: " + aulas.FirstOrDefault(aula => aula.Contains("Conclusão")));
 
+            aulas.Reverse();
+            Imprimir(aulas);
+
+            aulas.RemoveAt(aulas.Count - 1);
+            Imprimir(aulas);
+
+            aulas.Add("Conclusão");
+            Imprimir(aulas);
+
+            aulas.Sort();
+            Imprimir(aulas);
+
+            List<string> copia = aulas.GetRange(aulas.Count - 2, 2);
+            Imprimir(copia);
+
+            List<string> clone = new List<string>(aulas);
+            Imprimir(clone);
+
+            clone.RemoveRange(clone.Count - 2, 2);
+            Imprimir(clone);
 
             Console.ReadKey();
         }
