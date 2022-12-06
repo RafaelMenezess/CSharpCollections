2965b859a1c0915c1fe14d08ac90d6618394099c
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-05 21:00:08 -0300

    SortedList

---
+++ SortedList/Aluno.cs
@@ -0,0 +1,31 @@
+﻿namespace SortedList
+{
+    internal class Aluno
+    {
+        private string nome;
+        private int numeroMatricula;
+
+        public Aluno(string nome, int numeroMatricula)
+        {
+            this.nome = nome;
+            this.numeroMatricula = numeroMatricula;
+        }
+
+        public string Nome
+        {
+            get { return Nome; }
+            set { Nome = value; }
+        }
+
+        public int NumeroMatricula
+        {
+            get { return numeroMatricula; }
+            set { numeroMatricula = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"[Nome: {nome}, Matrícula: {numeroMatricula}";
+        }
+    }
+}
