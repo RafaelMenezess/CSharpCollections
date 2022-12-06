c7ce30d92a61d8eb1c84d0573312fa4295eca611
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-05 21:28:49 -0300

    SortedDictionary

---
+++ SortedDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+
+            Console.WriteLine();
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
