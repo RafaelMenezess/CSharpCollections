6e9b90cecce33c960eefe5fcb6c1a592a0d2a776
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-07 19:22:43 -0300

    JaggedArrays

---
+++ JaggedArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JaggedArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] familias = new string[3][];
+            //{
+            //    { "Fred","Wilma","Pedrita" },
+            //    { "Homer","Marge","Lisa","Bart","Marggie" },
+            //    { "Florinda","Kiko" }
+            //};
+
+            familias[0] = new string[] { "Fred", "Wilma", "Pedrita" };
+            familias[1] = new string[] { "Homer", "Marge", "Lisa", "Bart", "Marggie" };
+            familias[2] = new string[] { "Florinda", "Kiko" };
+
+            foreach (var familia in familias)
+            {
+                Console.WriteLine(string.Join(",", familia));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
