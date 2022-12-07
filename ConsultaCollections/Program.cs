e9bc58029e32ceb4129e6f0a681b1c3e9279c045
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-07 19:46:36 -0300

    ConsultandoCollections

---
+++ ConsultaCollections/Program.cs
@@ -0,0 +1,39 @@
+﻿using ConsultaCollections;
+using System;
+using System.Collections.Generic;
+
+namespace ConsultandoCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Mes> meses = new List<Mes>
+            {
+                new Mes("Janeiro", 31),
+                new Mes("Fevereiro", 28),
+                new Mes("Março", 28),
+                new Mes("Abril", 30),
+                new Mes("Maio", 31),
+                new Mes("Junho", 30),
+                new Mes("Julho", 31),
+                new Mes("Agosto", 31),
+                new Mes("Setembro", 30),
+                new Mes("Outubro", 31),
+                new Mes("Novembro", 30),
+                new Mes("Dezembro", 31)
+            };
+
+            meses.Sort();
+            foreach (var mes in meses)
+            {
+                if (mes.Dias == 31)
+                {
+                    Console.WriteLine(mes.Nome.ToUpper());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsultaCollections/mes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsultaCollections
+{
+    internal class Mes : IComparable
+    {
+        public Mes(string nome, int dias)
+        {
+            Nome = nome;
+            Dias = dias;
+        }
+
+        public string Nome { get; private set; }
+        public int Dias { get; private set; }
+
+        public int CompareTo(object obj)
+        {
+            Mes outro = obj as Mes;
+
+            return this.Nome.CompareTo(outro.Nome);
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Dias}";
+        }
+    }
+}
