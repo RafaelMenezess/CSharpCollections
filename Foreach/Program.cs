5a1d78bbb13dabf10c4a2c82c13747cf911480f4
Rafael Menezes <rafa.rcm@hotmail.com>
2022-12-12 21:11:36 -0300

    Foreach

---
+++ Foreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var meses = new List<string>
+            {
+                "Janeiro","Fevereiro","Março","Abril",
+                "Maio","Junho","Julho","Agosto","Setembro",
+                "Outubro","Novembro","Dezembro"
+            };
+
+            foreach (var item in meses)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
